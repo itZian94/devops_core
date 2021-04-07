93404cc426981189670605d2b3884272ad0f2012
ian <ianc199427@gmail.com>
2021-04-07 16:31:37 +0000

    Almost complete functionality on the application, waiting until other services are complete before finishing the first service. v0.1

--- service1/app.py
+++ service1/app.py
@@ -1,6 +1,32 @@
 from flask import Flask, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 
-import requests
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root@35.246.43.74/prize_generator'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
+db=SQLAlchemy(app)
+
+db.drop_all()
+db.create_all()
+
+class Key(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column('value', db.String(11), nullable=False)
+    result = db.Column('result', db.String(80), nullable=False)
+
+@app.route('/')
+def homepage():
+    keys = ""
+    return render_template("home.html", keys=keys)
+
+@app.route('/result')
+def result():
+    results = ""
+    results = Key.query.all()
+    return render_template("result.html", results=results)
+
+
+if __name__=='__main__':
+  app.run(host='0.0.0.0', debug=True)
