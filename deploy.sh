77b38799041d45d902e2ae1d3f957112450aba78
ian <ianc199427@gmail.com>
2021-04-11 18:26:29 +0000

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-scp -i ~/.ssh/id_rsa docker-compose.yaml jenkins@didact:home/jenkins/docker-compose.yaml
+scp -i ~/.ssh/id_rsa docker-compose.yaml jenkins@didact:./docker-compose.yaml
 ssh -i ~/.ssh/id_rsa jenkins@didact <<EOF
     export DATABASE_URI=${DATABASE_URI}
-    chmod ugo+rwx devops_core
     docker stack deploy --compose-file /home/jenkins/docker-compose.yaml devops_core
 EOF
