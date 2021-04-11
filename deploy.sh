4f241dd2d20f37034cdbe513a607d410bcef52e7
ian <ianc199427@gmail.com>
2021-04-11 13:12:09 +0000

    first build of complete pipeline

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@fundamental:/home/jenkins/docker-compose.yaml
+ssh -i ~/.ssh/ansible_id_rsa jenkins@fundamental <<EOF
+    export DATABASE_URI=${DATABASE_URI}
+    export SECRET_KEY=${SECRET_KEY}
+    docker stack deploy --compose-file /home/jenkins/docker-compose.yaml todo-stack
