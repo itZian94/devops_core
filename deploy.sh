3bc58d2dba45190fec63dc7d0b348fb3cbef8062
ian <ianc199427@gmail.com>
2021-04-11 13:45:19 +0000

    deploy script AGAIN V0.5

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@fundamental:./docker-compose.yaml
+scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@fundamental:home/jenkins/docker-compose.yaml
 ssh -i ~/.ssh/ansible_id_rsa jenkins@fundamental <<EOF
     export DATABASE_URI=${DATABASE_URI}
     export SECRET_KEY=${SECRET_KEY}
-    docker stack deploy --compose-file ./docker-compose.yaml devops_core
+    docker stack deploy --compose-file home/jenkins/docker-compose.yaml devops_core
 EOF
