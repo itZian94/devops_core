9915ca9d98f15861e8fe25d7cbc485651d5aaf3f
ian <ianc199427@gmail.com>
2021-04-11 14:50:59 +0000

    changed deploy script again again

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,5 @@
 scp -i ~/.ssh/id_rsa docker-compose.yaml jenkins@didact:home/jenkins/docker-compose.yaml
 ssh -i ~/.ssh/id_rsa jenkins@didact <<EOF
     export DATABASE_URI=${DATABASE_URI}
-    export SECRET_KEY=${SECRET_KEY}
-    docker stack deploy --compose-file home/jenkins/docker-compose.yaml devops_core
+    docker stack deploy --compose-file /home/jenkins/docker-compose.yaml devops_core
 EOF
